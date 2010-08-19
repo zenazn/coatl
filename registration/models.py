601e490d24129059933ccc024531b002b776f0be
Carl Jackson <carl@avtok.com>
2010-08-19 01:26:32 -0700

    I18N hooks in model labels
    Just in case we ever want to internationalize the site in the future, I
    wrapped a bunch of the labels I use in the models in I18N functionality.
    If nothing else, it gives us options in the future without adding
    terribly much overhead.

--- registration/models.py
+++ registration/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth import models as auth
 from competition import models as competition
+from django.utils.translation import ugettext_lazy as _
 
 SCHOOL_TYPES = (
-    ('private', 'Private School'),
-    ('magnet', 'Magnet School'),
-    ('regional', 'Regional School'),
-    ('public_big', 'Large Public School'),
-    ('public_small', 'Small Public School'),
-    ('individual', 'Individual'),
-    ('other', 'Other (E-mail us)'),
+    ('private', _('Private School')),
+    ('magnet', _('Magnet School')),
+    ('regional', _('Regional School')),
+    ('public_big', _('Large Public School')),
+    ('public_small', _('Small Public School')),
+    ('individual', _('Individual')),
+    ('other', _('Other (E-mail us)')),
 )
 
 TEAM_DIVISIONS = (
-    ('a', 'Division A'),
-    ('b', 'Division B'),
+    ('a', _('Division A')),
+    ('b', _('Division B')),
 )
 
 class School(models.Model):
@@ -27,15 +28,15 @@ class School(models.Model):
     
     coach = models.ForeignKey('auth.User')
 
-    created = models.DateField(auto_now_add=True, verbose_name="created at")
-    lastupdate = models.DateField(auto_now=True, verbose_name="last updated at")
+    created = models.DateField(auto_now_add=True, verbose_name=_('created at'))
+    lastupdate = models.DateField(auto_now=True, verbose_name=_('last updated at'))
     
     class Meta:
         db_table = 'schools'
     
 class Team(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    shortname = models.CharField(max_length=15, help_text="Nickname for the guts round heads-up", unique=True)
+    shortname = models.CharField(max_length=15, help_text=_('Nickname for the guts round heads-up'), unique=True)
     school = models.ForeignKey('School', db_column='schoolid')
     comment = models.TextField(null=True)
     proctor = models.CharField(max_length=255)
@@ -43,29 +44,29 @@ class Team(models.Model):
 
     division = models.CharField(max_length=1, choices=TEAM_DIVISIONS)
 
-    created = models.DateField(auto_now_add=True, verbose_name="created at")
-    lastupdate = models.DateField(auto_now=True, verbose_name="last updated at")
+    created = models.DateField(auto_now_add=True, verbose_name=_('created at'))
+    lastupdate = models.DateField(auto_now=True, verbose_name=_('last updated at'))
     
     class Meta:
         db_table = 'teams'
 
 class Mathlete(models.Model):
-    first = models.CharField(max_length=60, verbose_name="first name")
-    last = models.CharField(max_length=60, verbose_name="last name")
-    alias = models.CharField(max_length=60, null=True, verbose_name="nickname")
+    first = models.CharField(max_length=60, verbose_name=_('first name'))
+    last = models.CharField(max_length=60, verbose_name=_('last name'))
+    alias = models.CharField(max_length=60, null=True, verbose_name=_('nickname'))
     
     school = models.ForeignKey('School', db_column='schoolid')
     team = models.ForeignKey('Team', db_column='teamid')
     
-    round1 = models.ForeignKey('competition.Round', db_column='round1', related_name='mathelete_first_round_set', verbose_name="first round")
-    round2 = models.ForeignKey('competition.Round', db_column='round2', related_name='mathelete_second_round_set', verbose_name="second round")
+    round1 = models.ForeignKey('competition.Round', db_column='round1', related_name='mathelete_first_round_set', verbose_name=_('first round'))
+    round2 = models.ForeignKey('competition.Round', db_column='round2', related_name='mathelete_second_round_set', verbose_name=_('second round'))
 
-    regstatus = models.BooleanField(verbose_name="registration status")
+    regstatus = models.BooleanField(verbose_name=_('registration status'))
 
     comment = models.TextField(null=True)
     
-    created = models.DateField(auto_now_add=True, verbose_name="created at")
-    lastupdate = models.DateField(auto_now=True, verbose_name="last updated at")
+    created = models.DateField(auto_now_add=True, verbose_name=_('created at'))
+    lastupdate = models.DateField(auto_now=True, verbose_name=_('last updated at'))
     
     class Meta:
         db_table = 'mathletes'
