ef491646cd6c53dee2e98969c2ee6cdf3bfcb8db
Carl Jackson <carl@avtok.com>
2010-09-18 01:05:50 -0400

    Actually create objects in registration flow
    Also, there's some authentication logic in here too.

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,9 @@
-from coatl.registration import forms
+from coatl.registration import forms, models
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 # There's a three-part registration process. First you register an account,
@@ -12,8 +14,10 @@ def register_account(request):
     if request.method == 'POST':
         form = forms.RegisterUserForm(request.POST)
         if form.is_valid():
-            u = form.cleaned_data
-            User.objects.create_user(u['username'], u['email'], u['password'])
+            d = form.cleaned_data
+            User.objects.create_user(d['username'], d['email'], d['password'])
+            user = auth.authenticate(username=d['username'], password=d['password'])
+            auth.login(request, user)
             return HttpResponseRedirect("/registration/school")
     else:
         form = forms.RegisterUserForm()
@@ -23,10 +27,15 @@ def register_account(request):
     context.update(csrf(request))
     return render_to_response("registration/account.html", context)
  
+@login_required
 def register_school(request):
     if request.method == 'POST':
         form = forms.RegisterSchoolForm(request.POST)
         if form.is_valid():
+            s = models.School(**form.cleaned_data)
+            s.save()
+            s.coaches.add(request.user)
+            s.save()
             return HttpResponseRedirect("/registration/teams")
     else:
         form = forms.RegisterSchoolForm()
@@ -35,7 +44,8 @@ def register_school(request):
     }
     context.update(csrf(request))
     return render_to_response("registration/school.html", context)
- 
+
+@login_required
 def register_teams(request):
     if request.method == 'POST':
         teams = forms.TeamFormSet(request.POST)
