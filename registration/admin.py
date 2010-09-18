0d77a0456aa961dc57e97f4cbf6a38c3676ddee0
Carl Jackson <carl@avtok.com>
2010-09-18 01:37:47 -0400

    Admin for schools
    The admin for schools now includes a better data listing and a few
    useful filters

--- registration/admin.py
+++ registration/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from coatl.registration import models
 
 class SchoolAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'school_type', 'paid', 'payment', 'comment')
+    list_filter = ('paid', 'school_type')
 
 admin.site.register(models.School, SchoolAdmin)
 
