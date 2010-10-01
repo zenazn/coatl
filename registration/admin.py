b7a194161656c82bdee9f143002ce8af6e73a1f3
Carl Jackson <carl@avtok.com>
2010-10-01 12:41:42 -0400

    Search!
    Everyone loves search! Now you can search through all the relevant
    models. Yay!

--- registration/admin.py
+++ registration/admin.py
@@ -4,17 +4,20 @@ from coatl.registration import models
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ('name', 'school_type', 'paid', 'payment', 'comment')
     list_filter = ('paid', 'school_type')
+    search_fields = ('name', 'comment')
 
 admin.site.register(models.School, SchoolAdmin)
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'shortname', 'division', 'school', 'proctor', 'room', 'comment')
     list_filter = ('division', 'school')
+    search_fields = ('name', 'shortname', 'school__name', 'proctor', 'room', 'comment')
 
 admin.site.register(models.Team, TeamAdmin)
 
 class MathleteAdmin(admin.ModelAdmin):
     list_display = ('first', 'last', 'alias', 'school', 'round1', 'round2', 'comment')
     list_filter = ('round1', 'round2', 'school')
+    search_fields = ('first', 'last', 'alias', 'school__name', 'comment')
 
 admin.site.register(models.Mathlete, MathleteAdmin)
